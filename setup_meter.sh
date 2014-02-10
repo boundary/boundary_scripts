42d4127178349789ee98e6ecf03b2461f4d6a19a
Brent Cook <brent@boundary.com>
2014-02-10 17:26:44 -0600

    Use meter 2.0.2's built-in provisioning ability
    This simplifies the script a fair amount and makes future additions to
    the meter provisioning process easier as the logic is centralized in the
    package itself.

--- setup_meter.sh
+++ setup_meter.sh
@@ -53,10 +53,8 @@ map RHEL 6 Santiago
 # -----------------------------------------------------------------------------
 
 APIHOST="api.boundary.com"
+APICREDS=
 TARGET_DIR="/etc/bprobe"
-
-EC2_INTERNAL="http://169.254.169.254/latest/meta-data"
-TAGS="instance-type placement/availability-zone"
 
 SUPPORTED_ARCH=0
 SUPPORTED_PLATFORM=0
@@ -68,9 +66,7 @@ SMARTOS="smartos.boundary.com"
 APT_CMD="apt-get -q -y --force-yes"
 YUM_CMD="yum -d0 -e0 -y"
 
-DEPS="false"
 STAGING="false"
-CACERTS=
 
 trap "exit" INT TERM EXIT
 
@@ -87,6 +83,57 @@ function print_supported_platforms() {
         done
         echo ""
     done
+}
+
+EC2_INTERNAL="http://169.254.169.254/latest/meta-data"
+EC2_TAGS="instance-type placement/availability-zone"
+
+function ec2_find_tags() {
+    echo -n "Checking this is an ec2 environment..."
+    EC2=`curl -s --connect-timeout 5 "$EC2_INTERNAL"`
+    exit_code=$?
+
+    if [ "$exit_code" -eq "0" ]; then
+        echo "yes."
+        echo "Auto generating ec2 tags for this meter."
+    else
+        echo "no."
+        return 0
+    fi
+
+    METERTAGS=ec2
+
+    for tag in $EC2_TAGS; do
+        local AN_TAG
+        local exit_code
+
+        AN_TAG=`curl -s --connect-timeout 5 "$EC2_INTERNAL/$tag"`
+        exit_code=$?
+
+        # if the exit code is 7, that means curl couldnt connect so we can bail
+        # since we probably are not on ec2.
+        if [ "$exit_code" -eq "7" ]; then
+            # do nothing
+            return 0
+        fi
+
+        # it appears that an exit code of 28 is also a can't connect error
+        if [ "$exit_code" -eq "28" ]; then
+             # do nothing
+            return 0
+        fi
+
+        # otherwise, maybe there was as timeout or something, skip that tag.
+        if [ "$exit_code" -ne "0" ]; then
+            continue
+        fi
+
+        for an_tag in $AN_TAG; do
+            METERTAGS=$METERTAGS,$an_tag
+        done
+    done
+
+    echo Discovered ec2 tags: $METERTAGS
 }
 
 function check_distro_version() {
@@ -166,88 +213,15 @@ function print_help() {
     echo "   $0 [-d] -i ORGID:APIKEY"
     echo "      -i: Required input for authentication. The ORGID and APIKEY can be found"
     echo "          in the Account Settings in the Boundary WebUI."
-    echo "      -d: Optional flag to install all dependencies, such as curl and"
-    echo "          apt-transport-https, required for Meter Install."
     exit 0
 }
 
-function create_meter() {
-
-    if [ "${HOSTNAME}" = "" ]; then
-        echo "Host does not appear to have a hostname set!"
-        echo "For assistance, please contain support@boundary.com"
-        exit 1
-    fi
-
-    RESULT=`$CURL --connect-timeout 5 -i -s -X POST \
-        -H "Content-Type: application/json" \
-        -d "{\"name\": \"$HOSTNAME\"}" -u "$1:" \
-        https://$APIHOST/$2/meters \
-        | tr -d "\r" \
-        | awk '/^HTTP\/1\./ {split($0,a," "); http=a[2]} /^Location: https:\/\// {split($0,a," "); url=a[2]} END {print http; print url}'`
-
-    exit_status=$?
-
-    # an exit status of 1 indicates an unsupported protocol. (e.g.,
-    # https hasn't been baked in.)
-    if [ "$exit_status" -eq "1" ]; then
-        echo "Your local version of curl has not been built with HTTPS support: `which curl`"
-        exit 1
-
-    elif [ "$exit_status" -eq "6" ]; then
-        echo "Could not resolve $APIHOST, check network connectivity or DNS settings"
-        exit 1
-
-    # if the exit code is 7, that means curl couldnt connect so we can bail
-    elif [ "$exit_status" -eq "7" ]; then
-        echo "Could not connect to create meter"
-        exit 1
-
-    # it appears that an exit code of 28 is also a can't connect error
-    elif [ "$exit_status" -eq "28" ]; then
-        echo "Could not connect to create meter"
-        exit 1
-
-    elif [ "$exit_status" -ne "0" ]; then
-        echo "Error connecting to $APIHOST; status $exit_status."
-        exit 1
-    fi
-
-    STATUS=`echo $RESULT | awk '{print $1}'`
-
-    if [ "$STATUS" = "" ]; then
-        echo "Unknown error communicating with $APIHOST."
-        exit 1
-
-    elif [ "$STATUS" = "401" ]; then
-        echo "Authentication error, bad Org ID or API key (http status $STATUS)."
-        echo "Verify that you have passed in the correct credentials.  The ORGID and APIKEY"
-        echo "can be found in the Account Settings in the Boundary WebUI."
-        exit 1
-
-    elif [ "$STATUS" = "403" ]; then
-        echo "Forbidden error (http status $STATUS)."
-        echo "Verify that you have not exceeded your meter limit."
-        echo "If you haven't, please contact support at support@boundary.com."
-        exit 1
-
-    else
-        if [ "$STATUS" = "201" ] || [ "$STATUS" = "409" ]; then
-            echo $RESULT | awk '{print $2}'
-        else
-            echo "An Error occurred during the meter creation (http status $STATUS)."
-            echo "Please contact support at support@boundary.com."
-            exit 1
-        fi
-    fi
-}
-
 function do_install() {
-    if [ "$DISTRO" = "Ubuntu" ]; then
-        echo "Updating apt repository cache..."
-        $SUDO $APT_CMD update > /dev/null
-
-        APT_STRING="deb https://${APT}/ubuntu/ `get $DISTRO $MAJOR_VERSION.$MINOR_VERSION` universe"
+    if [ "$DISTRO" = "Ubuntu" ] || [ $DISTRO = "Debian" ]; then
+		APT_STRING="deb https://${APT}/ubuntu/ `get $DISTRO $MAJOR_VERSION.$MINOR_VERSION` universe"
+		if [ "$DISTRO" = "Debian" ]; then
+			APT_STRING="deb https://${APT}/debian/ `get $DISTRO $MAJOR_VERSION` main"
+		fi
         echo "Adding repository $APT_STRING"
         $SUDO sh -c "echo \"$APT_STRING\" > /etc/apt/sources.list.d/boundary.list"
 
@@ -259,26 +233,9 @@ function do_install() {
 
         echo "Updating apt repository cache..."
         $SUDO $APT_CMD update > /dev/null
-        $SUDO $APT_CMD install bprobe
+        $SUDO INSTALLTOKEN="${APICREDS}" PROVISIONTAGS="${METERTAGS}" $APT_CMD install bprobe
         return $?
-    elif [ "$DISTRO" = "Debian" ]; then
-        echo "Updating apt repository cache..."
-        $SUDO $APT_CMD update > /dev/null
 
-        APT_STRING="deb https://${APT}/debian/ `get $DISTRO $MAJOR_VERSION` main"
-        echo "Adding repository $APT_STRING"
-        $SUDO sh -c "echo \"$APT_STRING\" > /etc/apt/sources.list.d/boundary.list"
-
-        $CURL -s https://${APT}/APT-GPG-KEY-Boundary | $SUDO apt-key add -
-        if [ $? -gt 0 ]; then
-            echo "Error downloading GPG key from https://${APT}/APT-GPG-KEY-Boundary!"
-            exit 1
-        fi
-
-        echo "Updating apt repository cache..."
-        $SUDO $APT_CMD update > /dev/null
-        $SUDO $APT_CMD install bprobe
-        return $?
     elif [ "$DISTRO" = "CentOS" ] || [ $DISTRO = "Amazon" ] || [ $DISTRO = "RHEL" ]; then
         GPG_KEY_LOCATION=/etc/pki/rpm-gpg/RPM-GPG-KEY-Boundary
         if [ $MACHINE = "i686" ]; then
@@ -310,8 +267,9 @@ EOF"
             exit 1
         fi
 
-        $SUDO $YUM_CMD install bprobe
+        $SUDO INSTALLTOKEN="${APICREDS}" PROVISIONTAGS="${METERTAGS}" $YUM_CMD install bprobe
         return $?
+
     elif [ "$DISTRO" = "SmartOS" ]; then
       grep "http://${SMARTOS}/${MACHINE}" /opt/local/etc/pkgin/repositories.conf > /dev/null
 
@@ -320,143 +278,11 @@ EOF"
       fi
 
       pkgin -fy up
-      pkgin -y install bprobe
+      INSTALLTOKEN="${APICREDS}" PROVISIONTAGS="${METERTAGS}" pkgin -y install bprobe
       svccfg import /opt/custom/smf/boundary-meter.xml
       svcadm enable boundary/meter
       return $?
     fi
-}
-
-function setup_cert_key() {
-    trap "exit" INT TERM EXIT
-
-    test -d $TARGET_DIR
-
-    if [ $? -eq 1 ]; then
-        echo "Creating meter config directory ($TARGET_DIR) ..."
-        if [ $DISTRO = "SmartOS" ]; then
-            mkdir $TARGET_DIR
-        else
-            $SUDO mkdir $TARGET_DIR
-        fi
-    fi
-
-    test -f $TARGET_DIR/key.pem
-
-    if [ $? -eq 1 ]; then
-        echo "Key file is missing, attempting to download ..."
-        echo "Downloading meter key for $2"
-        if [ $DISTRO = "SmartOS" ]; then
-            $CURL -s -u $1: $2/key.pem | tee $TARGET_DIR/key.pem > /dev/null
-        else
-            $CURL -s -u $1: $2/key.pem | $SUDO tee $TARGET_DIR/key.pem > /dev/null
-        fi
-
-        if [ $? -gt 0 ]; then
-            echo "Error downloading key ..."
-            exit 1
-        fi
-
-        if [ $DISTRO = "SmartOS" ]; then
-            chmod 600 $TARGET_DIR/key.pem
-        else
-            $SUDO chmod 600 $TARGET_DIR/key.pem
-        fi
-    fi
-
-    test -f $TARGET_DIR/cert.pem
-
-    if [ $? -eq 1 ]; then
-        echo "Cert file is missing, attempting to download ..."
-        echo "Downloading meter certificate for $2"
-        if [ $DISTRO = "SmartOS" ]; then
-            $CURL -s -u $1: $2/cert.pem | tee $TARGET_DIR/cert.pem > /dev/null
-        else
-            $CURL -s -u $1: $2/cert.pem | $SUDO tee $TARGET_DIR/cert.pem > /dev/null
-        fi
-
-        if [ $? -gt 0 ]; then
-            echo "Error downloading certificate ..."
-            exit 1
-        fi
-
-        if [ $DISTRO = "SmartOS" ]; then
-            chmod 600 $TARGET_DIR/cert.pem
-        else
-            $SUDO chmod 600 $TARGET_DIR/cert.pem
-        fi
-    fi
-}
-
-function cert_key_check() {
-    SIZE=`du $TARGET_DIR/cert.pem | awk '{print $1}'`
-
-    if [ $SIZE -lt 1 ]; then
-        echo "Error downloading certificate (file size 0) ..."
-        exit 1
-    fi
-
-    SIZE=`du $TARGET_DIR/key.pem | awk '{print $1}'`
-
-    if [ $SIZE -lt 1 ]; then
-        echo "Error downloading key (file size 0) ..."
-        exit 1
-    fi
-}
-
-function ec2_tag() {
-    trap "exit" INT TERM EXIT
-
-    EC2=`$CURL -s --connect-timeout 5 "$EC2_INTERNAL"`
-    exit_code=$?
-
-    if [ "$exit_code" -eq "0" ]; then
-        # check to see if we *really* are on EC2
-        $CURL -is --connect-timeout 5 "$EC2_INTERNAL" | grep 'Server: EC2ws' > /dev/null
-        exit_code=$?
-
-        if [ "$exit_code" -eq "0" ]; then
-            echo -n "Auto generating ec2 tags for this meter...."
-        else
-            return 0
-        fi
-    else
-        return 0
-    fi
-
-    for tag in $TAGS; do
-        local AN_TAG
-        local exit_code
-
-        AN_TAG=`$CURL -s --connect-timeout 5 "$EC2_INTERNAL/$tag"`
-        exit_code=$?
-
-        # if the exit code is 7, that means curl couldnt connect so we can bail
-        # since we probably are not on ec2.
-        if [ "$exit_code" -eq "7" ]; then
-            # do nothing
-            return 0
-        fi
-
-        # it appears that an exit code of 28 is also a can't connect error
-        if [ "$exit_code" -eq "28" ]; then
-            # do nothing
-            return 0
-        fi
-
-    # otherwise, maybe there was as timeout or something, skip that tag.
-        if [ "$exit_code" -ne "0" ]; then
-            continue
-        fi
-
-        for an_tag in $AN_TAG; do
-            # create the tag
-            $CURL -H "Content-Type: application/json" -s -u "$1:" -X PUT "$2/tags/$an_tag"
-        done
-    done
-
-    $CURL -H "Content-Type: application/json" -s -u "$1:" -X PUT "$2/tags/ec2"
-    echo "done."
 }
 
 function pre_install_sanity() {
@@ -476,29 +302,22 @@ function pre_install_sanity() {
 
     which curl > /dev/null
     if [ $? -gt 0 ]; then
-        echo "The 'curl' command is either not installed or not on the PATH ..."
+		echo "Installing curl ..."
 
-        if [ $DEPS = "true" ]; then
-            echo "Installing curl ..."
+		if [ $DISTRO = "Ubuntu" ] || [ $DISTRO = "Debian" ]; then
+			echo "Updating apt repository cache..."
+			$SUDO $APT_CMD update > /dev/null
+			$SUDO $APT_CMD install curl
 
-            if [ $DISTRO = "Ubuntu" ] || [ $DISTRO = "Debian" ]; then
-                echo "Updating apt repository cache..."
-                $SUDO $APT_CMD update > /dev/null
-                $SUDO $APT_CMD install curl
+		elif [ $DISTRO = "CentOS" ] || [ $DISTRO = "Amazon" ] || [ $DISTRO = "RHEL" ]; then
+			if [ $MACHINE = "i686" ]; then
+				$SUDO $YUM_CMD install curl.i686
+			fi
 
-            elif [ $DISTRO = "CentOS" ] || [ $DISTRO = "Amazon" ] || [ $DISTRO = "RHEL" ]; then
-                if [ $MACHINE = "i686" ]; then
-                    $SUDO $YUM_CMD install curl.i686
-                fi
-
-                if [ $MACHINE = "x86_64" ]; then
-                    $SUDO $YUM_CMD install curl.x86_64
-                fi
-            fi
-        else
-            echo "To automatically install required components for Meter Install, rerun $0 with -d flag."
-            exit 1
-        fi
+			if [ $MACHINE = "x86_64" ]; then
+				$SUDO $YUM_CMD install curl.x86_64
+			fi
+		fi
     fi
 
     if [ $DISTRO = "SmartOS" ]; then
@@ -511,16 +330,10 @@ function pre_install_sanity() {
         test -f /usr/lib/apt/methods/https
         if [ $? -gt 0 ];then
             echo "apt-transport-https is not installed to access Boundary's HTTPS based APT repository ..."
-
-            if [ $DEPS = "true" ]; then
-                echo "Updating apt repository cache..."
-                $SUDO $APT_CMD update > /dev/null
-                echo "Installing apt-transport-https ..."
-                $SUDO $APT_CMD install apt-transport-https
-            else
-                echo "To automatically install required components for Meter Install, rerun $0 with -d flag."
-                exit 1
-            fi
+			echo "Updating apt repository cache..."
+			$SUDO $APT_CMD update > /dev/null
+			echo "Installing apt-transport-https ..."
+			$SUDO $APT_CMD install apt-transport-https
         fi
     fi
 }
@@ -589,12 +402,10 @@ else
 fi
 
 echo "Detected $DISTRO $VERSION..."
-echo ""
 
 while getopts "hdsi:f:" opts; do
     case $opts in
         h) print_help;;
-        d) DEPS="true";;
         s) STAGING="true";;
         i) APICREDS="$OPTARG";;
         f) echo "WARNING! You are OVERRIDING this script's OS detection."
@@ -714,33 +525,7 @@ fi
 # At this point, we think we have a supported OS.
 pre_install_sanity $d $v
 
-METER_LOCATION=`create_meter $APIKEY $APIID`
-
-if [ $? -gt 0 ]; then
-    echo "Error creating meter:"
-    echo "$METER_LOCATION"
-    echo "Please contact support@boundary.com"
-    exit 1
-fi
-
-KEY_CERT=`setup_cert_key $APIKEY $METER_LOCATION`
-if [ $? -eq 1 ]; then
-    echo "Error setting up cert and/or key ..."
-    echo "Please contact support@boundary.com"
-    echo $KEY_CERT
-    exit 1
-fi
-
-CERT_KEY_CHECK=`cert_key_check`
-
-if [ $? -eq 1 ]; then
-    echo "Error setting up cert and/or key ..."
-    echo "Please contact support@boundary.com"
-    echo $CERT_KEY_CHECK
-    exit 1
-fi
-
-ec2_tag $APIKEY $METER_LOCATION
+ec2_find_tags
 
 do_install
 
@@ -748,10 +533,6 @@ if [ $? -ne 0 ]; then
     echo "I added the correct repositories, but the meter installation failed."
     echo "Please contact support@boundary.com about this problem."
     exit 1
-fi
-
-if [ -n "$CACERTS" ]; then
-  rm -f $CACERTS
 fi
 
 echo ""
