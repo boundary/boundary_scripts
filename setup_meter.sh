bbf94f80aa73100cfa8277069c606d67455adad4
Pearce Barry <pearce@boundary.com>
2014-06-24 13:35:46 -0400

    Eschew SmartOS pkgin, prefer pkg_add.
    Per customer request, change the setup_meter.sh script to NOT update SmartOS repository information and use pkg_add to install the meter.  See BOUNDARY-1393 and BOUNDARY-3485.

--- setup_meter.sh
+++ setup_meter.sh
@@ -293,14 +293,8 @@ EOF"
         return $?
 
     elif [ "$DISTRO" = "SmartOS" ]; then
-      grep "http://${SMARTOS}/${MACHINE}" /opt/local/etc/pkgin/repositories.conf > /dev/null
-
-      if [ "$?" = "1" ]; then
-        echo "http://${SMARTOS}/${MACHINE}/" >> /opt/local/etc/pkgin/repositories.conf
-      fi
-
-      pkgin -fy up
-      pkgin -y install boundary-meter
+      SMARTOS_PKG=$(curl -s http://${SMARTOS}/${MACHINE}/ | sed -n 's/^.*\"\(boundary-meter.*tgz\)\".*$/\1/p' | tail -n 1)
+      pkg_add -f -v http://${SMARTOS}/${MACHINE}/${SMARTOS_PKG}
       # Enable promiscuous mode on SmartOS by default.
       # Non-promiscuous mode is not very useful because the OS only forwards
       # received traffic.
