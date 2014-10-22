0a59d534be2f20de0989129569f954750a1578b7
Brent Cook <busterb@gmail.com>
2014-10-22 14:31:47 -0500

    add support for FreePBX / Schmooze
    Centos 5/6 variant

--- setup_meter.sh
+++ setup_meter.sh
@@ -17,7 +17,7 @@ set -o pipefail
 ### limitations under the License.
 ###
 
-PLATFORMS=("Ubuntu" "Debian" "CentOS" "Amazon" "RHEL" "SmartOS" "openSUSE" "FreeBSD" "LinuxMint" "Gentoo" "Oracle" "Scientific")
+PLATFORMS=("Ubuntu" "Debian" "CentOS" "Amazon" "RHEL" "SmartOS" "openSUSE" "FreeBSD" "LinuxMint" "Gentoo" "Oracle" "Scientific" "SHMZ")
 
 # Put additional version numbers here.
 # These variables take the form ${platform}_VERSIONS, where $platform matches
@@ -34,6 +34,7 @@ LinuxMint_VERSIONS=("13", "14", "15", "16")
 Gentoo_VERSIONS=("1.12.11.1")
 Oracle_VERSIONS=("5" "6" "7")
 Scientific_VERSIONS=("6" "7")
+SHMZ_VERSIONS=("5" "6")
 
 # sed strips out obvious things in a version number that can't be used as
 # a bash variable
@@ -218,7 +219,7 @@ function check_distro_version() {
         MINOR_VERSION=`echo $VERSION | awk -F. '{print $2}'`
         VERSION_CMP=$MAJOR_VERSION.$MINOR_VERSION
 
-    elif [ $DISTRO = "CentOS" ] || [ $DISTRO = "RHEL" ] || [ $DISTRO = "Oracle" ] || [ $DISTRO = "Scientific" ]; then
+    elif [ $DISTRO = "CentOS" ] || [ $DISTRO = "RHEL" ] || [ $DISTRO = "Oracle" ] || [ $DISTRO = "Scientific" ] || [ $DISTRO = "SHMZ" ]; then
         MAJOR_VERSION=`echo $VERSION | awk -F. '{print $1}'`
         VERSION_CMP=$MAJOR_VERSION
 
@@ -316,7 +317,7 @@ function do_install() {
         zypper install -f -y boundary-meter
         return $?
 
-    elif [ "$DISTRO" = "CentOS" ] || [ $DISTRO = "Amazon" ] || [ $DISTRO = "RHEL" ] || [ $DISTRO = "Oracle" ] || [ $DISTRO = "Scientific" ]; then
+    elif [ "$DISTRO" = "CentOS" ] || [ $DISTRO = "Amazon" ] || [ $DISTRO = "RHEL" ] || [ $DISTRO = "Oracle" ] || [ $DISTRO = "Scientific" ] || [ $DISTRO = "SHMZ" ]; then
         GPG_KEY_LOCATION=/etc/pki/rpm-gpg/RPM-GPG-KEY-Boundary
         if [ "$MACHINE" = "i686" ]; then
             ARCH_STR="i386/"
@@ -388,7 +389,7 @@ function pre_install_sanity() {
             $APT_CMD update > /dev/null
             $APT_CMD install curl
 
-        elif [ $DISTRO = "CentOS" ] || [ $DISTRO = "Amazon" ] || [ $DISTRO = "RHEL" ] || [ $DISTRO = "Oracle" ] || [ $DISTRO = "Scientific" ]; then
+        elif [ $DISTRO = "CentOS" ] || [ $DISTRO = "Amazon" ] || [ $DISTRO = "RHEL" ] || [ $DISTRO = "Oracle" ] || [ $DISTRO = "Scientific" ] || [ $DISTRO = "SHMZ" ]; then
             if [ "$MACHINE" = "i686" ]; then
                 $YUM_CMD install curl.i686
             elif [ "$MACHINE" = "x86_64" ]; then
@@ -455,6 +456,8 @@ if [ -f /etc/redhat-release ] ; then
                 DISTRO="RHEL"
                 VERSION=`echo $PLATFORM | awk '{print $7}'`
            elif [ "$DISTRO" = "Scientific" ]; then
+                VERSION=`echo $PLATFORM | awk '{print $4}'`
+           elif [ "$DISTRO" = "SHMZ" ]; then
                 VERSION=`echo $PLATFORM | awk '{print $4}'`
            else
                 DISTRO="unknown"
